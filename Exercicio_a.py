80f609453c86c412c34cee29aa280ee4e1fa62f4
Beatriz Cradoso <beatriz.cardoso@eq.ufrj.br>
2024-11-02 12:42:13 -0300

    Update Exercicio_a.py

--- Exercicio_a.py
+++ Exercicio_a.py
@@ -1,4 +1,4 @@
-#Exercicio a
+#Exercicio 1-a
 var1 = """Python é uma linguagem de programação de alto nível, [5] interpretada de script, imperativa, orientada
 a objetos, funcional, de tipagem dinâmica e forte. Foi lançada por Guido van Rossum em 1991.[1] Atualmente, possui
 um modelo de desenvolvimento comunitário, aberto e gerenciado pela organização sem fins lucrativos Python Software
